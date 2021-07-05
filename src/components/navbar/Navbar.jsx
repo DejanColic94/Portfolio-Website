52613e882ec9fd9787c9db288c785a4bdb83a4fa
Dejan Colic <dejan.colic99@gmail.com>
2021-07-05 22:52:56 +0200

    icons in the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import './Navbar.scss';
+import {Person,Mail} from "@material-ui/icons";
+
 
 export default function Navbar() {
     return (
@@ -6,7 +8,14 @@ export default function Navbar() {
             <div className="wrapper">
                 <div className="left">
                     <a href="#intro" className="logo">logo</a>
-                    
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>+381 63 290958</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon"/>
+                        <span>dejan.colic94@gmail.com</span>
+                    </div>
                 </div>
                 
                 <div className="right">
